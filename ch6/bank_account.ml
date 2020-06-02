a0c1debca3b31984d4b4dad934284cd83e5e5ce9
YueLiPicasso <yue.li.picasso@qq.com>
2020-06-02 16:03:12 +0800

    extended object example: bank account

--- ch6/bank_account.ml
+++ ch6/bank_account.ml
@@ -88,3 +88,53 @@ ccp # history;;
         
 (* close account *)
 let close c = c#withdraw c#balance;;
+
+let today () = (01,01,2000)
+
+
+(* Account functor abstracted over some currency *)
+
+module Account (M:MONEY) =
+struct
+  type 'a operation = Deposit of 'a | Retrieval of 'a;;
+  type m = M.c
+  let m = new M.c
+  let zero = m 0.
+  let neg (x : M.c) = x # times (-1.)
+
+  class bank =
+    object (self)
+      val mutable balance = zero
+      method balance = balance
+      val mutable history = []
+      method private trace x = history <- x :: history
+      method deposit x =
+        self # trace (Deposit x);
+        if zero # leq x then balance <- balance # plus x
+        else raise (Invalid_argument "deposit")
+      method withdraw x =
+        if x#leq balance then
+          (balance <- balance # plus (neg x); self # trace (Retrieval x); x)
+        else zero
+      method history = List.rev history
+    end
+
+  class type client_view =
+    object
+      method deposit : m -> unit
+      method history : m operation list
+      method withdraw : m -> m
+      method balance : m
+    end
+
+  class virtual check_client x =
+    let y = if (m 100.)#leq x then x
+      else raise (Failure "Insufficient initial deposit") in
+    object (self)
+      initializer self # deposit y
+      method virtual deposit : m -> unit
+    end
+
+  (* module Client (B : sig class bank : client_view end)*)
+end;;
+
