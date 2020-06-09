aee5d6c1595720320d7eb207c8faa542aaba7dcd
YueLiPicasso <yue.li.picasso@qq.com>
2020-06-09 11:56:50 +0800

    GT intro

--- ch8/extensions.ml
+++ ch8/extensions.ml
@@ -6,18 +6,12 @@ let rec oz = 1 :: zo and zo = 0 :: oz in (oz, zo);;
 *)
 
 (* GT examples *)
+
+(* a (custom) type *)
 type expr =
   | Const of int
   | Var of string
   | Binop of string * expr * expr;;
-
-let rec show = function
-  | Const n -> "Const " ^ string_of_int n
-  | Var x -> "Var " ^ x
-  | Binop (o, l, r) ->
-    Printf.sprintf "Binop (%S, %s, %s)" o (show l) (show r);;
-
-print_string (show (Binop ("+", Const 1, Var "x")));;
 
 (* simple priority assignment *)
 let prio = function
@@ -28,66 +22,140 @@ let prio = function
   | "exp" -> 5
   | s -> raise (Invalid_argument s);;
 
+(* polymorphic identity function*)
 let id : 'a. 'a -> 'a = fun x -> x;;
 
+(* add parentheses *)
 let br = fun (s : string) -> "( " ^ s ^ " )";;
 
 br "hello";;
 
-let pretty e =
-  let rec pretty_prio p = function
-    | Const n -> string_of_int n
-    | Var x -> x
+
+module ExprPrint =
+struct
+  (* a type-indexed function *)
+  let rec show'expr = function
+    | Const n -> "Const " ^ string_of_int n
+    | Var x -> "Var " ^ x
     | Binop (o, l, r) ->
-      let po = prio o in
-      (if po <= p then br else id) @@
-      pretty_prio po l ^ " " ^ o ^ " " ^ pretty_prio po r
-  in
-  pretty_prio min_int e;;
+      Printf.sprintf "Binop (%S, %s, %s)" o (show'expr l) (show'expr r);;
+  
+  (* another function indexed by type expr *)
+  let pretty'expr e =
+    let rec pretty_prio p = function
+      | Const n -> string_of_int n
+      | Var x -> x
+      | Binop (o, l, r) ->
+        let po = prio o in
+        (if po <= p then br else id) @@
+        pretty_prio po l ^ " " ^ o ^ " " ^ pretty_prio po r
+    in
+    pretty_prio min_int e;;
+end;;
 
-pretty (Binop ("exp", (Binop ("*", (Const 4), (Var "x"))),
-               (Binop ("-", (Var "y"), (Binop ("+",(Const 4),(Var "z")))))));; 
 
-(* extract pattern matching from "pretty" and "show" *)
+(* Extracting pattern matching from "pretty" and "show", 
+   we get the traversal function gcata'expr indexed by the 
+   type expr. It takes three parameters: 
+   -- omega is a transformation object
+   -- iota is an extra parameter that can be used or ignored; 
+   it corresponds to the priority value used by pretty_prio
+   -- a value of type expr
+ *)
 
-let gcata omega iota = function
+let gcata'expr omega iota = function
   | Const n -> omega # pConst iota n
   | Var x -> omega # pVar iota x
   | Binop (o, l, r) -> omega # pBinop iota o l r;;
-(* omega is a transformation object *)
-(* iota is an extra parameter that can be used or ignored *)
 
-let rec show' e =
-  let transobj =
-  object
-    method pConst _ n = "Const " ^ string_of_int n
-    method pVar _ x = "Var " ^ x
-    method pBinop _ o l r =
-      Printf.sprintf "Binop (%S, %s, %s)" o (show' l) (show' r)
-  end
-  in gcata transobj () e;;
+(* we can redefine show'expr and pretty'expr using 
+   the traversal function gcata'expr for type expr 
+   with custom transformation objects
+*)
+
+(* using an immediate object *)
+module ExprImO =
+struct
+  let rec show'expr e =
+    let transobj =
+      object
+        method pConst _ n = "Const " ^ string_of_int n
+        method pVar _ x = "Var " ^ x
+        method pBinop _ o l r =
+          Printf.sprintf "Binop (%S, %s, %s)" o (show'expr l) (show'expr r)
+      end
+    in gcata'expr transobj () e;;
   
-print_string (show' (Binop ("+", Const 1, Var "x")));;
-
-
-class virtual ['iota, 'sigma] expr_trans =
+  let pretty'expr e =
+    let rec pretty_prio p e' =
+      let transobj = object
+        method pConst _ n = string_of_int n
+        method pVar _ x = x
+        method pBinop p o l r = let po = prio o in
+          (if po <= p then br else id) @@
+          pretty_prio po l ^ " " ^ o ^ " " ^ pretty_prio po r
+      end in gcata'expr transobj p e'
+    in pretty_prio min_int e;;
+end;;
+      
+(* a transformation object can also be obtained from
+   instances of a virtual transformation class *)
+class virtual ['iota, 'sigma] transformation'expr =
   object
     method virtual pConst : 'iota -> int -> 'sigma
     method virtual pVar : 'iota -> string -> 'sigma
     method virtual pBinop : 'iota -> string -> expr -> expr -> 'sigma
   end;;
 
-(*
-let rec pretty' e =
-  let transobj =
-    object (self)
-      method pConst _ n = string_of_int n
-      method pVar _ x -> x
-      method pBinop p o l r = let po = prio o in
-      (if po <= p then br else id) @@
-      pretty' l ^ " " ^ o ^ " " ^ pretty' r
-    end
-  in
-  gcata transobj min_int e;;*)
 
-        
+module ExprCls =
+struct
+  (* a concrete transformation class for show'expr *)
+  (* a self-trasformation function fself facilitates recursive calls *)
+  class show (fself : expr -> string) =
+    object
+      inherit [unit, string] transformation'expr
+      method pConst _ n = "Const " ^ string_of_int n
+      method pVar _ x = "Var " ^ x
+      method pBinop _ o l r =
+        Printf.sprintf "Binop (%S, %s, %s)" o (fself l) (fself r)
+    end;;
+
+  (* concrete transformation class *)
+  class pretty (fself : 'iota -> expr -> 'sigma) =
+    object
+      inherit [int, string] transformation'expr
+      method pConst _ n = string_of_int n
+      method pVar _ x = x
+      method pBinop p o l r =
+        let po = prio o in
+        (if po <= p then br else id) @@
+        fself po l ^ " " ^ o ^ " " ^ fself po r
+    end;;
+
+  let rec show'expr e = gcata'expr (new show show'expr) () e;;
+  
+  let pretty'expr e =
+    let rec pretty_prio p e = gcata'expr (new pretty pretty_prio) p e in
+    pretty_prio min_int e;;
+
+end;;
+
+(* tests *)
+print_string ExprPrint.(show'expr (Binop ("+", Const 1, Var "x")));;
+ExprPrint.pretty'expr
+  (Binop ("exp", (Binop ("*", (Const 4), (Var "x"))),
+          (Binop ("-", (Var "y"),
+                  (Binop ("+",(Const 4),(Var "z")))))));; 
+
+print_string ExprImO.(show'expr (Binop ("+", Const 1, Var "x")));;
+ExprImO.pretty'expr
+  (Binop ("exp", (Binop ("*", (Const 4), (Var "x"))),
+          (Binop ("-", (Var "y"),
+                  (Binop ("+",(Const 4),(Var "z")))))));;
+
+print_string ExprCls.(show'expr (Binop ("+", Const 1, Var "x")));;
+ExprCls.pretty'expr
+  (Binop ("exp", (Binop ("*", (Const 4), (Var "x"))),
+          (Binop ("-", (Var "y"),
+                  (Binop ("+",(Const 4),(Var "z")))))));; 
