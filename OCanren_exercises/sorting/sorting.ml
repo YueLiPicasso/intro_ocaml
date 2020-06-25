22239ff96c46542ac338ad5f05ddbba76dbbd2c8
YueLiPicasso <yue.li.picasso@qq.com>
2020-06-25 16:31:47 +0800

    wrapped minmaxo

--- OCanren_exercises/sorting/sorting.ml
+++ OCanren_exercises/sorting/sorting.ml
@@ -50,7 +50,7 @@ let rec smallesto l s l' =
    provide routines to convert data from GT-level to 
    groundl-level via ground-level and logic-level.
 *)
-
+(* The type error massages could be helpful for debugging *)
 
 (* Raise an int list from ground-level to GT-level *)
 (* Nat.to_int    : Nat.ground -> int *)
@@ -63,12 +63,12 @@ let tofun_int_list = List.to_list Nat.to_int;;
 let fromfun_int_list = nat_list;; (* OCanren.nat_list *)
 
 
-(* Raise an int * (int list) pair from ground-level to GT-level *)
-let liil_pair_prj pr = Nat.to_int (fst pr), tofun_int_list (snd pr);;
-
-(* wraping samllesto to interface with functional data *)
+(* wrapping samllesto to interface with GT-level data *)
 (* Using Core.qr for two logical parameters *)
-let smallest l = liil_pair_prj @@ Stream.hd @@
+let smallest l =
+  (* Raise an int * (int list) pair from ground-level to GT-level *)
+  let raise pr = Nat.to_int (fst pr), tofun_int_list (snd pr) in
+  raise @@ Stream.hd @@
   run qr
     (* the goal, which returns reified results *)
     (fun s l' -> smallesto (fromfun_int_list l) s l')
@@ -80,11 +80,18 @@ let _ = let pr = smallest [4;3;2;1;5;6;7;8] in
   Printf.printf "The smallest is %s and the remainder is: %s\n%!"
      (show(GT.int) (fst pr)) (show(GT.list) (show(GT.int)) (snd pr));; 
 
-(* wrap minmaxo in a functional context: 
-   provide min and max, returns a and b *)
-(* Use OCanren.Std.nat to inject OCaml int to 
-   LNat.ground and finally to logic domain*)
+
+(* wrapping minmaxo to interface with GT-level data, 
+   The user provides min and max, minmaxr returns a pair (a, b) *)
+(* Use OCanren.Std.nat to push int from GT-level to groundl-level *)
 let minmaxr min max =
-  Stream.take ~n:2 @@
-  run qr (fun a b -> minmaxo (nat a) (nat b) min max)
-    (fun as bs -> as#prj, bs#prj)
+  (* Raise the (int * int) part of (int * int) list from 
+     ground-level to GT-level; the enclosing list itself
+     is already in GT-level *)
+  let raise pl =
+    (* Raise an (int * int) from ground-level to GT-level *)
+    let raise' (ft, st) = (Nat.to_int ft), (Nat.to_int st) in
+    L.map raise' pl in
+  raise @@ Stream.take ~n:2 @@
+  run qr (fun a b -> minmaxo  a  b (nat min) (nat max))
+    (fun ast bs -> ast#prj, bs#prj);;
