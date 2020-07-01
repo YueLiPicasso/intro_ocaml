c1f690353326e4a59ffa5188dad36ed24b3457a8
YueLiPicasso <yue.li.picasso@qq.com>
2020-07-01 11:35:09 +0800

    jeep problem introduced

--- OCanren_exercises/JeepProblem/jeep.ml
+++ OCanren_exercises/JeepProblem/jeep.ml
@@ -5,26 +5,32 @@ The Jeep Problem
 https://mathworld.wolfram.com/JeepProblem.html 
 
 A jeep is going to cross a desert: moving from base A on one side
-of the desert to base B on the other side. There is no
-fuel station along the way. The jeep has one fuel tank, and it 
-carries empty bottles:  on the way it can 
-transfer some fuel from the tank to some bottles and leave those 
-bottles there. The jeep may go back to base A to refuel, or refuel 
-in the desert from previoulsy left bottles.
+of the desert to base B on the other side. 
 
-
-We abstract from particular details of the jeep. For instance,  the jeep's 
-tank may actually hold 4.67 liters of fuel maximum  and with which it 
-can run 412.6 km. We take 4.67 liters as one unit of fuel
+The jeep has a fuel tank. We abstract from certain specifications of 
+the tank. For instance,  the tank may hold 10.67 liters of fuel maximum
+and with which it can run 412.6 km. We take 10.67 liters as one unit of fuel
 and take 412.6 km as one unit of distance, and say that the jeep's 
 tank holds one unit of fuel, and with which it can run for one unit of 
 distance.
 
+There is a fuel station in base A, providing practically unlimited units 
+of fuel. The distance between base A and B is much longer
+than one unit, but unfortunately there is no more fuel stations along the way,
+and at any time the jeep can carry at most one unit of fuel.
 
-The total distance of the journey, if we want the problem to be non-trivial,
-shall be longer than one unit, so that the jeep must unload fuel on the way,
-i.e.,  creating some fuel dump sites in the desert, and refuel from both
-base A and the fuel dump sites in order to reach base B.  
+ 
+The specific strategy that jeep uses to cross the desert in this situation
+is that it carries empty bottles:  on the way it could transfer some fuel 
+from the tank to some bottles and leave the bottles there, thus creating 
+mini fuel dumps in the desert. The jeep could refuel by going back to base 
+A or from the fuel dumps.  
+
+
+The jeep problem can be formulated as: how should the jeep create dumps and 
+refuel in order to complete the journey, and  what is the minimum amount of 
+fuel needed by the jeep ?   
+
 
 *)
 
