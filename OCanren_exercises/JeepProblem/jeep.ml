e26859435a077875b7bfbde591ce1452f3ef49ab
YueLiPicasso <yue.li.picasso@qq.com>
2020-07-03 18:33:13 +0800

    jeep: loopup and put dumps

--- OCanren_exercises/JeepProblem/jeep.ml
+++ OCanren_exercises/JeepProblem/jeep.ml
@@ -188,8 +188,79 @@ and backward x =  !! (Backward x)
 and unload x = !! (Unload x)
 and fill x = !! (Fill x);;
 
-  
+
+(* Given the position d of the jeep, see if there is any fuel dump 
+   there, by searching for d in an association list l consisting of 
+   <fuel dump position>-<dump fuel amount> pairs, the indices of 
+   which are sorted in ascending order. If there is a dump, return
+   the fuel amount fl there (wrapped in the 'Some' value constrcutor), 
+   otheriwse return None. *)
 
 
+let rec lookupo d l fl =
+  let open Nat in
+  ocanren {
+    l == [] & fl == None |
+    fresh p', fl', l' in
+      l == (p', fl') :: l' &
+    { d == p' & Some fl' == fl   |
+      d >  p' & lookupo d l' fl |
+      d <  p' & fl == None }}
+;;
+
+(* val lookupo : 
+   Nat.groundi ->
+  ((Nat.ground, 'a) Pair.ground, (Nat.logic, 'b) Pair.logic) List.groundi ->
+  ('a Option.ground, 'b GT.option logic) injected ->
+  OCanren.goal  
+
+   Note that
+ 
+   ((Nat.ground, 'a) Pair.ground, (Nat.logic, 'b) Pair.logic) List.groundi
+   
+   expands to
+ 
+   ((Nat.ground, 'a) Pair.ground List.ground, 
+   (Nat.logic, 'b) Pair.logic List.logic) injected 
+   
+   whose first parameter is a ground-level list of ground-level pairs of
+   ground-level nats with 'a. *)
+
+
+(* test if a number is positive *)
+
+
+let positive n = ocanren { fresh x in n == Nat.succ x };;
+
+
+(* Reset the fuel dump at p to fl units of fuel, thus turning
+   an exsting ordered list l of dumps into a new list ln. 
+   The rules are: 
+   - if the existing list of dumps is empty, then:
+     - if the new fuel amount is 0, then nothing is done;
+     - if the new fuel amount is not 0 (positive), then creata a new dunp;
+   - if the first existing dump is (p_1,fl_1), and the rest are l_res, 
+     then:
+     - if just this dump is to be reset (p == p_1), then:
+       - if the new amount is 0, then this dump is simply removed from the list;
+       - if the new amount is not 0 (positive), then set the fuel to this new amount
+     - if p is after p_1 (p > p_1), then resett p to fl in l_res to get ln_res, 
+       and ln is (p_1,fl_1)::ln_res;
+     - if p is before p_1, then:
+       - if the new fuel amount is 0, then nothing is done;
+       - if the new fuel amount is not 0 (positive), then prefix a dump (p,fl) to l. 
+*)
+
+
+let rec reseto p fl l ln =
+  let open Nat in
+  ocanren {
+    l == [] & { fl == 0 & ln == [] | positive fl & ln == [(p,fl)] } |
+    fresh p_1, fl_1, l_res, ln_res in
+l == (p_1,fl_1)::l_res &
+{ { p == p_1 & { fl == 0 & ln == l_res | positive fl & ln == (p,fl)::l_res} } |
+  { p >  p_1 & reseto p fl l_res ln_res & ln == (p_1,fl_1)::ln_res }          |
+  { p <  p_1 & {fl == 0 & l == ln | positive fl & ln == (p,fl)::l } }
+} }
 
 
