230994a54a498c04f90b4b5d99ae5cf4170bbcf5
YueLiPicasso <yue.li.picasso@qq.com>
2020-07-23 14:51:30 +0800

    debug

--- OCanren_exercises/Gold_Mining/mining.ml
+++ OCanren_exercises/Gold_Mining/mining.ml
@@ -9,11 +9,9 @@ module Rat = LRational;;
 
 module Machine = struct
   open Rat;;
-  
   (* machine performance on A *)
   let p = inj_int_ratio (1,5)
   and r = inj_int_ratio (1,2);;
-
   (* machine performance on B *)
   let q = inj_int_ratio (3,10)
   and s = inj_int_ratio (33,100);;
@@ -22,40 +20,16 @@ end;;
 module Mine = struct
   let a = !!A         (* injected *)
   and b = !!B;;
-  
   open Rat;;
-  
   let x = inj_int_ratio (100,1)     (* init amount in A *)
   and y = inj_int_ratio (120,1);;   (* init amount in B *)
 end;;
 
-let select m a b sel =
-  ocanren {
-    m == Mine.a & a == sel |
-    m == Mine.b & b == sel
-  };;
-
-
-let rec expectation amt_A amt_B plan (expc : Rat.groundi)=
+let expectation amt_A amt_B plan (expc : Rat.groundi) =
   ocanren {
     plan == [] & expc == (0,1) |
-    fresh m, ms,pr, fr,
-          amt_now, amt_mined, amt_left in
-      plan == m :: ms                  &
-      select m Machine.p Machine.q pr  &
-      select m Machine.r Machine.s fr  &
-      select m amt_A amt_B amt_now     &
-      Rat.( * ) fr amt_now amt_mined   &
-      Rat.( - ) amt_now amt_mined amt_left & 
-    fresh summ, expc_ms, expc_ms', expc_ms'' in
-     Rat.( + ) amt_mined expc_ms summ &
-     Rat.( * ) pr summ expc           &
-     select m expc_ms' expc_ms'' expc &
-     expectation amt_left amt_B ms expc_ms' 
-     expectation amt_A amt_left ms expc_ms'' (* this double recursion is a source 
-                                                of inefficiency *)
-};;
-
+    fresh m, ms in plan == m :: ms &
+      expecctation'' m amt_A amt_B ms expc };;
 
 (* Expectation for (hd_plan :: tl_plan) *)
 
@@ -79,8 +53,7 @@ let rec expectation' hd_plan amt_A amt_B tl_plan (expc : Rat.groundi)=
            & Rat.( + ) amt_mined expc_tl summ
            & Rat.( * ) Machine.q summ expc } } };;
 
-
-(* Expectation for (hd_plan :: tl_plan) *)
+(* Expectation for (hd_plan :: tl_plan), clauses reordered *)
 
 let rec expectation'' hd_plan amt_A amt_B tl_plan (expc : Rat.groundi)=
   ocanren {
@@ -100,11 +73,9 @@ let rec expectation'' hd_plan amt_A amt_B tl_plan (expc : Rat.groundi)=
         fresh m, ms in tl_plan == m :: ms &
           fresh amt_left, expc_tl, summ in
             Rat.( - ) amt_B amt_mined amt_left
-          & expectation' m  amt_A amt_left ms expc_tl
+          & expectation'' m  amt_A amt_left ms expc_tl
           & Rat.( + ) amt_mined expc_tl summ
           & Rat.( * ) Machine.q summ expc } };;
-
-
 
 (* some tests *)
 
