b4aa200b4aea88d78088f6c5edf266d6479bd40c
YueLiPicasso <yue.li.picasso@qq.com>
2020-08-17 12:19:59 +0800

    move ahead

--- OCanren_exercises/DCSyn/dcsyn.ml
+++ OCanren_exercises/DCSyn/dcsyn.ml
@@ -1,20 +1,23 @@
+(** A relational translator *)
 open OCanren;;
 module L = List;;
 open OCanren.Std;;
 
-(** {1 Types for the imperative language} *)
 
-(** use integer or string as ['a] *)
-@type 'a var = 'a with show, gmap, eq, compare;;
+(** shared syntactic categories *)
+
+@type 'a var = 'a with show, gmap, eq, compare;; (** use integer or string as ['a] *)
 
 @type 'var expr = Low | High | Var of 'var with show, gmap, eq, compare;;
 
+(** syntactic categories unique to the imperative language *)
+
 @type ('var, 'expr, 'stat ) stat = If_then_else 'expr * 'stat * 'stat
-                                 | ColonEq 'var * 'expr
+                                 | Assign 'var * 'expr
  with show, gmap, eq, compare;;
 
 @type ('a, 'p) prog = ('a, 'p ) List.t with show, gmap, eq, compare;;
 
 
-(** {1 Types for the imperative language} *)
+(** syntactic categories unique to the flowchart language *)
 
