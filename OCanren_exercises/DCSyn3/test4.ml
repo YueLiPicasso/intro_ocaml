a8ea1a59ba4fedc314f2538d6296be709a020514
YueLiPicasso <yue.li.picasso@qq.com>
2020-09-29 15:39:37 +0800

    I/O generator

---
+++ OCanren_exercises/DCSyn3/test4.ml
@@ -0,0 +1,20 @@
+(** This file tests ... *)
+open OCanren;;
+module L = List ;;
+open OCanren.Std;;
+open Dcsyn3;;
+open Interp;;
+open Interp.NoLet;;
+open TwoBit;;
+
+@type svp = State.ground * Value.ground with show;;
+(* produce all I/O pairs for a given imperative program *)
+let _ =
+  L.iter (fun x -> print_string @@ GT.show(svp) x;print_newline())
+  @@ Stream.take  @@ 
+  run q (fun q -> ocanren {fresh prog, vs, sts,res in
+    prog == Brh(Var "x", Brh(Arr("y", Var "x"), Con c1, Con c2), Con c0)
+    & Expr.free_var prog vs
+    & Expr.var_state vs sts
+    & eval_imp sts prog res
+    & q == (sts, res)}) project;;
