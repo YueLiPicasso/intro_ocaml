b7ceb95fd4424d61081e9ada9e213f15f8eeb200
YueLiPicasso <yue.li.picasso@qq.com>
2020-10-02 11:10:18 +0800

    partial success

--- OCanren_exercises/DCSyn3/test4.ml
+++ OCanren_exercises/DCSyn3/test4.ml
@@ -7,9 +7,10 @@ open Interp;;
 open Interp.NoLet;;
 open TwoBit;;
 
-(** compute all possible input/output combinations, viz., I/O pairs, of the given imperative program *)
+(** compute all possible input/output combinations, viz., I/O pairs, of the given imperative program, 
+but take the first [n] of them; the default is to take all 1024 *)
 let specs : Spec.ground GT.list =  
-  Stream.take  @@ 
+  Stream.take ~n:41  @@ 
   run q (fun q -> ocanren {fresh prog, vs, sts,res in
     prog == Brh(Var "x", Brh(Arr("y", Var "x"), Con c1, Con c2), Con c0)
     & Expr.free_var prog vs
@@ -17,7 +18,7 @@ let specs : Spec.ground GT.list =
     & eval_imp sts prog res
     & q == (sts, res)}) project;;
 
-(** Print all the I/O pairs *)
+(** Print the I/O pairs *)
 let _ =
   L.iter (fun x -> print_string @@ GT.show(Spec.ground) x;print_newline()) specs;;
 
@@ -27,11 +28,26 @@ let _ = Printf.printf "Synthesizing from %d input-output pairs...\n" (L.length s
 (** convert the I/O pairs from ground to groundi *)
 let specsi : Specs.groundi = Specs.grd2ijd specs;;
 
-(** synthesize a flowchart program that satisfies all the I/O pairs *) 
+(** synthesize a flowchart program that satisfies the I/O pairs *) 
 let _ =
   L.iter (fun x -> print_string @@ GT.show(Signal.logic) x;print_newline())
   @@ Stream.take ~n:1 @@
   run q (fun q -> ocanren {syn specsi q}) (fun q -> q#reify(Signal.reify))
+
+
+(*
+The result of synthesizing from 41 input-output pairs:
+
+Mux (Port ("x"), Mux (Slice (Port ("y"), Src ((I, O))), Src ((O, I)), Src ((I, O))), Src ((O, O)))
+
+The structure is already very similar to the imperative program, and the only difference is the index of [Slice].
+ 
+The 42ed I/O pair is:
+ 
+([("x", Conv ((I, I))); ("y", Arrv ((((O, O), (O, O)), ((O, O), (I, O)))))], Conv ((O, I)))
+
+which causes the killed process. This means that the search was along a wrong direction.  
+*)
 
 
 
