bbe97f02070239ad47c2923aeecabab9b32051f6
YueLiPicasso <yue.li.picasso@qq.com>
2020-10-10 18:35:29 +0800

    trie type

--- OCanren_exercises/simpleTrie/trie.ml
+++ OCanren_exercises/simpleTrie/trie.ml
@@ -1,11 +1,39 @@
 (** THe type of a particular trie *)
+open Logic;;
 open ExpoTuples;;
 
-@type t = {table : Register.t Expo2.t; alpha: Cell.t} with show, gmap;;
+@type 'a logic' = 'a logic with show, gmap;; 
 
-@type ground = {table : Register.ground Expo2.ground; alpha : Cell.ground} with show, gmap;;
+module T = struct
+  @type ('a,'b) t = {table : 'a ; alpha : 'b} with show, gmap;;
+  let fmap = fun f1 f2 x -> GT.gmap(t) f1 f2 x;;
+end;;
 
-@type logic = {table : Register.logic Expo2.logic; alpha : Cell.logic} with show, gmap;;
+include T;;
 
-type groundi = {table : (Register.ground,Register.logic) Expo2.groundi; alpha : Cell.groundi};;
+@type ground = (Register.ground Expo2.ground,  Cell.ground) t with show, gmap;;
+
+@type logic = (Register.logic Expo2.logic, Cell.logic) t logic' with show, gmap;;
+
+type groundi = (ground, logic) injected;;
+
+module FT :
+sig
+  val distrib :
+    (('a, 'c) Logic.injected, ('b, 'd) Logic.injected) T.t ->
+    (('a, 'b) T.t, ('c, 'd) T.t) Logic.injected
+  val reify :
+    (VarEnv.t -> ('a, 'b) Logic.injected -> 'b) ->
+    (VarEnv.t -> ('c, 'd) Logic.injected -> 'd) ->
+    VarEnv.t ->
+    (('a, 'c) T.t, ('b, 'd) T.t Logic.logic) Logic.injected ->
+    ('b, 'd) T.t Logic.logic
+  val prjc :
+    (VarEnv.t -> ('a, 'b) Logic.injected -> 'a) ->
+    (VarEnv.t -> ('c, 'd) Logic.injected -> 'c) ->
+    (int -> ('a, 'c) T.t list -> ('a, 'c) T.t) ->
+    VarEnv.t ->
+    (('a, 'c) T.t, ('b, 'd) T.t Logic.logic) Logic.injected -> ('a, 'c) T.t
+end
+  = Fmap2(T);;
 
