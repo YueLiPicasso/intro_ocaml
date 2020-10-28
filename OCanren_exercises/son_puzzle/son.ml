a7213ac5ef5ac397b88753b3483cf84ab1121e60
YueLiPicasso <yue.li.picasso@qq.com>
2020-10-28 23:47:59 +0800

    puzzle

--- OCanren_exercises/son_puzzle/son.ml
+++ OCanren_exercises/son_puzzle/son.ml
@@ -1,8 +1,4 @@
-(** A puzzle for kids. 
-    Ask a boy "who is the son of your mother which
-    is not your brother?"
-    Similarly, ask a girl "who is the daughter of your mother
-    which is not your sister?"*)
+
 
 open OCanren;;
 
