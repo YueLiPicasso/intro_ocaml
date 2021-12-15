b1ca7bcf24685fd3f0741c117d4b6ea2e70e0cce
YueLiPicasso <yue.li.picasso@qq.com>
2021-12-15 14:26:03 +0300

    dune and Makefile dual support

---
+++ learnM/yue_eucpp/README.md
@@ -0,0 +1,14 @@
+# Reifiers Written in the Monad Pattern
+
+Adapted without conceptual change from [Moiseenko](https://gist.github.com/eupp/a78e9fc086834106e98d50e1e7bdea24).
+
+## Tips
+
+A functional programmer is not born to know category theory but is very likely to be tempted 
+to look at this field when being repeatedly confronted by codes that
+feature the "monad" programming pattern, because monad origins from category theory (See [Wadler](https://homepages.inf.ed.ac.uk/wadler/topics/monads.html)). On the one hand, I tried several available resources on category but only to realise that there is no answer quick and good for the question "What is category, what is monad and why they are interesting?", and at the moment I still do not have a grasp of the field. On the other hand, I find it helpful to build a mental firewall in my head between "functional programs in the monad pattern" and "monad as part of category theory", so that the curiosity inspired by the former for the latter is kept in check, and to be content with looking at these programs solely as pieces of ordinary definitions to be read and understood as is and without the categorial canotations. 
+
+## To Build
+
+Use either Dune or Makefile . 
+
--- learnM/yue_eucpp/test/test.ml
+++ learnM/yue_eucpp/test/test.ml
@@ -1,20 +1,18 @@
 open Printf
-open Eucpp    
-open Eucpp.Core
+open Eucpp
+open Eucpp.Core    
 
 let _ = match Reifier.(apply reify @@ runrt ()) with
   | Value _ -> printf "failed\n"
   | Var _ -> printf "PASSED\n"
-
-let _ = match Reifier.(apply reify @@ runfr ()) with
+and _ = match Reifier.(apply reify @@ runfr ()) with
   | Value _ -> printf "failed\n"
   | Var _ -> printf "PASSED\n"
-
-let _ = match Reifier.(apply reify @@ run (fun _ -> Env.return @@ inj 44)) with
+and _ = match Reifier.(apply reify @@ run (fun _ -> Env.return @@ inj 44)) with
   | Value 44 -> printf "PASSED\n"
   | _ ->  printf "failed\n"
 
-let _ =        
+let _ =
   let runaway : int ilogic ref = ref (Obj.magic ()) in  
   let state_ok = run (fun v -> runaway := v; Env.return v) in
   try Reifier.(apply reify @@ run (fun _ -> Env.return !runaway))   
@@ -22,20 +20,21 @@ let _ =
                | Var.Scope_violation _ -> printf "PASSED\n";
                                           Reifier.(apply reify state_ok)
                                          
-let _ = print_string @@ List.fold_right
-          (fun v s ->
-            s ^ "(" ^
-              string_of_int ((Obj.magic (Var.index v)) : int)
-              ^
-                " , "
-              ^
-                string_of_int ((Obj.magic (Var.env v))   : int)
-              ^
-                ")\n")
-          Var.[fresh (fresh_env ());fresh (fresh_env ());
-               fresh (fresh_env ());
-               fresh (fresh_env ());fresh (fresh_env ());
-               fresh (fresh_env ())]
-         ""
+let _ = 
+  print_string @@ List.fold_right
+    (fun v s ->
+       s ^ "(" ^
+       string_of_int ((Obj.magic (Var.index v)) : int)
+       ^
+       " , "
+       ^
+       string_of_int ((Obj.magic (Var.env v))   : int)
+       ^
+       ")\n")
+    Var.[fresh (fresh_env ());fresh (fresh_env ());
+         fresh (fresh_env ());
+         fresh (fresh_env ());fresh (fresh_env ());
+         fresh (fresh_env ())]
+    ""
 (* [expr1 ; expr2; expr3 ; ... ; exprn] evalutation order 
    exprn , ..., expr3, expe2, expr1. *)
