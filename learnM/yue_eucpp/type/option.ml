c190c666a68fb9987a353cf2de7e241f4f49d533
YueLiPicasso <yue.li.picasso@qq.com>
2021-12-20 12:54:18 +0300

    complete type annotation for Option.reify

--- learnM/yue_eucpp/type/option.ml
+++ learnM/yue_eucpp/type/option.ml
@@ -12,22 +12,39 @@ let fmap = fun f -> function
   | None -> None
 
 let reify = fun (ra : ('a ->'b) Env.t) ->
-  (Env.bind :
-     ('a2 Core.ilogic -> 'a2 Core.logic) Env.t
-   -> (('a2 Core.ilogic -> 'a2 Core.logic) -> 'b3 Env.t)
-   -> 'b3 Env.t)
-    (Reifier.reify : ('a2 Core.ilogic -> 'a2 Core.logic) Env.t )
-    (fun r ->
-       ((Env.bind : ('a ->'b) Env.t -> (('a ->'b) -> 'b1 Env.t) -> 'b1 Env.t)
-          (ra : ('a ->'b) Env.t)
-          ((fun (fa : 'a -> 'b) ->
-             ((Env.return
-               ((fun x ->
-                  match r x with
-                  | Var v as v' -> v'
-                  | Value t -> Value (fmap fa t)
-                ) : 'b1)
-              ) : 'b1 Env.t)
-            ) : ('a ->'b) -> 'b1 Env.t )
-       )
-    )
+  (((Env.bind :
+       ('a1 Core.ilogic -> 'a1 Core.logic) Env.t
+     -> (('a1 Core.ilogic -> 'a1 Core.logic) -> ('a ilogic -> 'b logic) Env.t)
+     -> ('a ilogic -> 'b logic) Env.t)
+      (Reifier.reify : ('a1 Core.ilogic -> 'a1 Core.logic) Env.t ))
+   : (('a1 Core.ilogic -> 'a1 Core.logic) -> ('a ilogic -> 'b logic) Env.t)
+   -> ('a ilogic -> 'b logic) Env.t)
+    ((fun (r : 'a1 Core.ilogic -> 'a1 Core.logic) ->
+        ((((((Env.bind : ('a ->'b) Env.t -> (('a ->'b) -> ('a ilogic -> 'b logic) Env.t)
+              -> ('a ilogic -> 'b logic) Env.t)
+               (ra : ('a ->'b) Env.t))
+            : (('a ->'b) -> ('a ilogic -> 'b logic) Env.t) -> ('a ilogic -> 'b logic) Env.t)
+             ((fun (fa : 'a -> 'b) ->
+                 ((Env.return
+                     ((fun (x : 'a t Core.ilogic) ->
+                         match (r x : 'a t Core.logic) with
+                         | Var v  as v' -> (v' : 'b t Core.logic)
+                         | Value t -> ((Value ((fmap (fa : 'a -> 'b) (t : 'a t)) : 'b t))
+                                       : 'b t Core.logic)
+                       ) : 'a ilogic -> 'b logic)
+                  ) : ('a ilogic -> 'b logic) Env.t)
+               ) : ('a ->'b) -> ('a ilogic -> 'b logic) Env.t )
+          )) : ('a ilogic -> 'b logic) Env.t)
+      ) :  ('a1 Core.ilogic -> 'a1 Core.logic) -> ('a ilogic -> 'b logic) Env.t)
+    
+(*
+ let reify : ('a, 'b) Reifier.t -> ('a ilogic, 'b logic) Reifier.t = fun ra ->
+    let (>>=) = Env.bind in
+    (Reifier.reify >>= (fun r -> (ra >>= (fun fa ->
+      Env.return (fun x ->
+        match r x with
+        | Var v   -> Var v
+        | Value t -> Value (fmap fa t)
+      )
+    ))))
+*)
